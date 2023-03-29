53c0eb5b754c0aa064bb3afe16d569a08ef708aa
seeweq <sivuyisiwet34@gmail.com>
2023-03-29 11:29:39 +0200

    first register commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {RouterOutlet} from "@angular/router";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
